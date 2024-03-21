dd00286f54c13497c05452654396246a7555cca5
Pavel.S <nils010493@yandex.ru>
2024-03-21 19:32:36 +0500

    second commit

---
+++ comp.js
@@ -0,0 +1,186 @@
+Vue.component("change-kitchen", {
+  template: ` <div class="article-subContent">
+  <h2 class="article-content__heading">
+    Let’s Get Solution for Building Construction Work
+  </h2>
+  <img
+    width="800px"
+    class="article-subContent__banner"
+    src="./img/detailsImg1.png"
+    alt=""
+  />
+  <div class="article-subContent-data">
+    <p>26 December,2022</p>
+    <p>Interior / Home / Decore</p>
+  </div>
+  <div class="article-subContent__text">
+    <p>
+      Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae
+      turpmaximus.posuere in.Contrary to popular belief.There are many
+      variations of passages of Lorem Ipsum available, but the majority
+      have suffered alteration in some form, by injecthumour, or
+      randomised words which don't look even slightly believable.
+    </p>
+    <p>
+      Embarrassing hidden in the middle of text. All the Lorem Ipsum
+      generators on the Internet tend to repeat predefined chunks as
+      necessary.
+    </p>
+  </div>
+</div>`,
+});
+Vue.component("change-bedroom", {
+  template: ` <div class="article-subContent">
+  <h2 class="article-content__heading">
+    Let’s Get Solution for Building Construction Work
+  </h2>
+  <img
+    width="800px"
+    class="article-subContent__banner"
+    src="./img/cart3.png"
+    alt=""
+  />
+  <div class="article-subContent-data">
+    <p>26 December,2022</p>
+    <p>Interior / Home / Decore</p>
+  </div>
+  <div class="article-subContent__text">
+    <p>
+      Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae
+      turpmaximus.posuere in.Contrary to popular belief.There are many
+      variations of passages of Lorem Ipsum available, but the majority
+      have suffered alteration in some form, by injecthumour, or
+      randomised words which don't look even slightly believable.
+    </p>
+    <p>
+      Embarrassing hidden in the middle of text. All the Lorem Ipsum
+      generators on the Internet tend to repeat predefined chunks as
+      necessary.
+    </p>
+  </div>
+</div>`,
+});
+Vue.component("change-building", {
+  template: ` <div class="article-subContent">
+  <h2 class="article-content__heading">
+    Let’s Get Solution for Building Construction Work
+  </h2>
+  <img
+    width="800px"
+    class="article-subContent__banner"
+    src="./img/cart2.png"
+    alt=""
+  />
+  <div class="article-subContent-data">
+    <p>26 December,2022</p>
+    <p>Interior / Home / Decore</p>
+  </div>
+  <div class="article-subContent__text">
+    <p>
+      Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae
+      turpmaximus.posuere in.Contrary to popular belief.There are many
+      variations of passages of Lorem Ipsum available, but the majority
+      have suffered alteration in some form, by injecthumour, or
+      randomised words which don't look even slightly believable.
+    </p>
+    <p>
+      Embarrassing hidden in the middle of text. All the Lorem Ipsum
+      generators on the Internet tend to repeat predefined chunks as
+      necessary.
+    </p>
+  </div>
+</div>`,
+});
+Vue.component("change-architecture", {
+  template: ` <div class="article-subContent">
+  <h2 class="article-content__heading">
+    Let’s Get Solution for Building Construction Work
+  </h2>
+  <img
+    width="800px"
+    class="article-subContent__banner"
+    src="./img/cart1.png"
+    alt=""
+  />
+  <div class="article-subContent-data">
+    <p>26 December,2022</p>
+    <p>Interior / Home / Decore</p>
+  </div>
+  <div class="article-subContent__text">
+    <p>
+      Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae
+      turpmaximus.posuere in.Contrary to popular belief.There are many
+      variations of passages of Lorem Ipsum available, but the majority
+      have suffered alteration in some form, by injecthumour, or
+      randomised words which don't look even slightly believable.
+    </p>
+    <p>
+      Embarrassing hidden in the middle of text. All the Lorem Ipsum
+      generators on the Internet tend to repeat predefined chunks as
+      necessary.
+    </p>
+  </div>
+</div>`,
+});
+Vue.component("change-kitchen planning", {
+  template: ` <div class="article-subContent">
+  <h2 class="article-content__heading">
+    Let’s Get Solution for Building Construction Work
+  </h2>
+  <img
+    width="800px"
+    class="article-subContent__banner"
+    src="./img/detailsImg1.png"
+    alt=""
+  />
+  <div class="article-subContent-data">
+    <p>26 December,2022</p>
+    <p>Interior / Home / Decore</p>
+  </div>
+  <div class="article-subContent__text">
+    <p>
+      Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae
+      turpmaximus.posuere in.Contrary to popular belief.There are many
+      variations of passages of Lorem Ipsum available, but the majority
+      have suffered alteration in some form, by injecthumour, or
+      randomised words which don't look even slightly believable.
+    </p>
+    <p>
+      Embarrassing hidden in the middle of text. All the Lorem Ipsum
+      generators on the Internet tend to repeat predefined chunks as
+      necessary.
+    </p>
+  </div>
+</div>`,
+});
+Vue.component("change-bedrom", {
+  template: ` <div class="article-subContent">
+  <h2 class="article-content__heading">
+    Let’s Get Solution for Building Construction Work
+  </h2>
+  <img
+    width="800px"
+    class="article-subContent__banner"
+    src="./img/cart3.png"
+    alt=""
+  />
+  <div class="article-subContent-data">
+    <p>26 December,2022</p>
+    <p>Interior / Home / Decore</p>
+  </div>
+  <div class="article-subContent__text">
+    <p>
+      Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae
+      turpmaximus.posuere in.Contrary to popular belief.There are many
+      variations of passages of Lorem Ipsum available, but the majority
+      have suffered alteration in some form, by injecthumour, or
+      randomised words which don't look even slightly believable.
+    </p>
+    <p>
+      Embarrassing hidden in the middle of text. All the Lorem Ipsum
+      generators on the Internet tend to repeat predefined chunks as
+      necessary.
+    </p>
+  </div>
+</div>`,
+});
